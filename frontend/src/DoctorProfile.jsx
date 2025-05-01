1b0356e05bbfa29026df04dbde43f4a13771618d
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-05-01 12:34:06 +0530

    Update DoctorProfile.jsx

--- frontend/src/DoctorProfile.jsx
+++ frontend/src/DoctorProfile.jsx
@@ -34,14 +34,11 @@ const DoctorProfile = () => {
             <p>
               Looking for a dentist committed to your care? Reach out to us today and schedule a visit!
             </p>
-            <a
-            href="https://book.modento.io/c/e3c519c43483402da1f510401a487e28/Koneru/patient-details"
-            className="cta-button"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            REQUEST APPOINTMENT
-          </a>
+             <div className="cta-phone">📱 (919)-851-9690</div>
+              <a href="tel:+19198519690" className="cta-button">
+              REQUEST APPOINTMENT
+            </a>
+          </div>
           </div>
         </div>
       </div>
