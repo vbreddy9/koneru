b76142f4f9c3b00984b01ef0c8c6d20e874a7a09
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-05-16 16:18:07 +0530

    Update Footer.jsx

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Mouthhealer. All Rights Reserved. -<a href="https://www.mouthhealer.com/privacy-policy/"> Privacy Policy</a>- Design by Wizard Technologies</p>
+      <p>&copy; 2025 Korenu Familiy Dentistry. All Rights Reserved. -<a href="https://www.mouthhealer.com/privacy-policy/"> Privacy Policy</a>- Design by Wizard Technologies</p>
     </footer>
   );
 };
